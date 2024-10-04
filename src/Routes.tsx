0122a538c80134ccdd70f3c77995266e5e482066
athena272 <guilhermera272@gmail.com>
2024-10-04 11:00:59 -0300

    Update Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { RootStackParamList } from './@types'
 import Tabs from './screens/Tabs'
+import Login from './screens/Login'
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
 
@@ -9,6 +10,7 @@ export default function Routes() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
+                <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
                 <Stack.Screen name='Tabs' component={Tabs} options={{ headerShown: false }} />
             </Stack.Navigator>
         </NavigationContainer>
